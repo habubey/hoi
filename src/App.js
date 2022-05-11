c172dd2c72e6012086ecc124a481cdc4b9458336
habubey <habubey@outlook.com>
2022-05-11 18:53:13 +0300

    minimze

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
-
+import background from "./Images/pexels-karolina-grabowska-4590785.jpg"
 
 
 
 function App() {
   return (
-    <div>
+    <div style={{ 
+      backgroundImage: `url(${background})` 
+    }}>
       <Navbar/>
       <Header/>
        
